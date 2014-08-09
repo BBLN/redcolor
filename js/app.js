5f42e7e2321d55c5c87ff11b3c2f3c433ed3f9d5
BBLN <doomclanserv@gmail.com>
2014-08-09 22:21:08 +0300

    Switched to tzeva adom api

--- js/app.js
+++ js/app.js
@@ -4,132 +4,216 @@ App.Router.map(function() {
   // put your routes here
 });
 
+App.Alert = Ember.Object.extend();
+
+App.Alert = DS.Model.extend({
+  area_id: DS.attr('number'),
+  area_name: DS.attr('string'),
+  time:  DS.attr('date'),
+  area_lat: DS.attr('number'),
+  area_long: DS.attr('number'),
+});
+
+App.Alert.reopenClass({
+  all: function() {
+        return $.getJSON('http://tzeva-adom.com/alerts.php?fmt=jsonp&source=pikud,test&limit=5&callback=?').then(function(response) {
+            console.log(App.Alert.create(response))
+            return null;
+        });
+    },
+
+    expireTime: 6,
+
+    create: function ()
+    {
+        var items = this._super(arguments);
+        // Expiring.
+        setTimeout(function expiredAlert() {
+            console.log(items);
+            items[0].forEach(function removeAlert(alert)
+                {
+                    console.log("Alert expired: " + alert);
+                    alert.delete();
+                });
+        }, App.Alert.expireTime * 1000);
+
+        return items;
+    },
+});
+
 App.IndexRoute = Ember.Route.extend({
    model: function() {
-    return this.store.all('color');
-  }
-});
+        console.log("Model");
+        data = App.Alert.all();
 
-App.Color = DS.Model.extend({
-  section: DS.attr('string'), /* דן */
-  code: DS.attr('number'), /* 153 */
-});
+        var alerts = new Alerts("http://tzeva-adom.com:8080/redalert");
+        alerts.callback(function(response) { 
+            response.forEach(function (item) {
+                items.push( App.Alert.create(item) );
+            });
+        });
+        alerts.poll();
 
-/* Taken from walla. */
-
-/* Lazy call */
-// bind a function to a scope, returns a function, use with event listeners
-function bind(func,scope) {
-    var f = function() {
-        func.apply(scope,arguments);
-    }
-    return f;
-}
-
-/* After massive walla cleanup. Still WIP. */
-oref = {
-    config : {
-        url: "http://192.118.82.227/wos", /* Thanks walla */
-        services: "oref",
-        connection: null,
-        privacy: true, /* Privacy++ */
-        defaultUrl: "http://walla.co.il/",
-    },
-
-    send : function(msg)
-    {
-        msg.orData = (this.config.privacy) ? this.config.defaultUrl : window.location.href;
-        if (this.config.connection &&
-           this.config.connection.readyState === SockJS.OPEN)
-        {
-            var data = JSON.stringify(msg);
-            this.config.connection.send(data);
-            console.log("Message sent: " + data);
-        }
-        else
-        {
-            console.log("Unable to connect to server");
-        }
-    },
-
-    /* subscribe / unsubscribe */
-    register : function(un){
-        if(typeof un == "undefined") 
-            var msg = { subscribe : this.config.services };
-        else
-            var msg = { unsubscribe : this.config.services };
-
-        this.send(msg);
-    },
-
-    init : function(callback) {
-        this.config.connection = new SockJS(this.config.url);
-
-        this.config.connection.onopen = bind(function(){
-            this.register();
-        }, this);
-
-        this.config.connection.onmessage = bind(function(message) {
-            var obj = JSON.parse(message.data);
-            var type = message.type;
-
-            callback(obj);
-        }, this);
-    },
-}
-
-App.ApplicationRoute = Ember.Route.extend({
-  model: function() {
         var original_background = $("body").css("background-image");
-        oref.init(function (data)
-            {
-                /* TODO: Replace with my own parser. Walla sucks */
-                var orAlerts = [];
-                var rssentries=data.data.rss.channel[0].item || [];
+        var opacity = (1 + data.length) / 10; 
+        if (opacity > 1.0)
+            opacity = 1.0; /* MAX */
+        $("body").css("background-image", "linear-gradient(rgba(255, 0, 0, " + opacity + "),  rgba(255, 0, 0, " + opacity + "))," + original_background);
+        return data;
+    },
+});
 
-                if (rssentries.length > 0)
-                {
-                    var mainLabel = data.data.rss.channel[0].title[0];
-                    try {
-                        var descLabel = data.metadata[2];
-                    }
-                    catch(e) {
-                        var descLabel= "";
-                    }
+App.LiveMapView = Ember.View.extend({
+    classNames: ['live-map'], 
+    templateName: 'live-map',
 
-                    // map the data needed to a new array
-                    for(var i=0; i<rssentries.length; i++){
-                        orAlerts[i] = {
-                            priority: rssentries[i]["alerts:priority"][0],
-                            title: rssentries[i].title[0],
-                            description: rssentries[i].description[0]
-                        };
-                    }
+    // Ember is awesome.
+    didInsertElement: function()
+    {
+        /* Initialize map */
+        var map = new google.maps.Map(this.$().get(0), {
+                zoom: 10,
+                center: new google.maps.LatLng(31.4, 34.5),
+                mapTypeId: google.maps.MapTypeId.ROADMAP
+        });
 
-                    // sort by priority
-                    orAlerts.sort(function(a, b){
-                        if(a.priority < b.priority) return 1;
-                        if(a.priority > b.priority) return -1;
-                        return 0;
-                    });
+        this.set("map", map); /* Useful for inserting circles */
+        this.contentDidChange();
+    },
 
-                    for(var i=0; i<orAlerts.length; i++)
-                    {
-                        console.log(orAlerts[i].priority);
-                        console.log(orAlerts[i].title);
-                        console.log(orAlerts[i].description);
-                    }
+    contentDidChange: function() {
+        var map = this.get("map");
+
+        this.get('controller.content').forEach(function alertCircle(alert)
+        {
+            /* Thanks to redalert.co.il */
+            var circle = new google.maps.Circle({
+                strokeColor: "red",
+                strokeWeight: 1,
+                fillColor: "red",
+                fillOpacity: 0,
+                map: map,
+                center: new google.maps.LatLng( alert.area_lat, alert.area_long ),
+                radius: 0
+            });
+
+            // expand animation
+            var opacity = .5;
+            var expand = function() {
+                var radius = circle.get( "radius" ) + 1500;
+                circle.set( "radius", radius );
+                if ( radius < 3000 ) {
+                    return requestAnimationFrame( expand );
                 }
 
-                this.store.push('color', {
-                  section: 'דן',
-                  code: 153
-                });
+                setTimeout( function() {
+                    circle.set( "fillOpacity", opacity );
+                    circle.set( "strokeWeight", 0 );
+                    fade();
+                }, 300 );
+            }
+            requestAnimationFrame( expand );
 
-                var opacity = (1 + this.store.all("color").length) / 10; 
-                if (opacity > 1.0)
-                    opacity = 1.0; /* MAX */
-                $("body").css("background-image", "linear-gradient(rgba(255, 0, 0, " + opacity + "),  rgba(255, 0, 0, " + opacity + "))," + original_background);
-            });
-    }
+            var fade = function() {
+                var opacity = circle.get( "fillOpacity" ) - (( window.location.hash == "#slow" ) ? 0.005 : 0.01);
+                if ( window.location.hash == "#nofade" && opacity > 0 && opacity < 0.1 ) {
+                    return; // no more fading
+                }
+
+
+                circle.set( "fillOpacity", opacity );
+                if ( opacity <= 0 ) {
+                    return circle.setMap( null );
+                }
+                requestAnimationFrame( fade );
+            }
+        });
+      }.observes('controller.content'),
 });
+
+// Gadi Cohen, nov12, GPLed.   v0.1
+// See http://tzeva-adom.com/alerts.html
+
+/**
+  * Alerts class constructor.  Initiates a new Alerts object, which handles
+  * JSONP long polling to the server (see below).
+  * @constructor
+  *
+  * @param url    Defaults to http://currenthost:8080/redalert
+  * <br>          You'll want http://tzeva-adom.com:8080/redalert on external code
+  *
+  * @return Alerts object
+  */
+function Alerts(url) {
+    this.timeout = 0;
+    this.callbacks = [];
+    this.sleeptime = 1000;
+    if (!url) url = "http://" + window.location.host + ":8080/redalert";
+    this.url = url;
+    this.lastSuccess = null;
+    this.failureCount = 0;
+}
+
+/**
+  * Specify a callback function to be called whenever there is new data
+  * from the server.  The function will be called with the new data as it's
+  * only argument.  You may add as many callback functions as required.
+  *
+  * @param callback   - function reference or anonymous function
+  *
+  */
+Alerts.prototype.callback = function(callback) {
+    this.callbacks.push(callback);
+}
+
+/**
+  * Performs a JSONP "long poll" (a "reverse ajax" technique)
+  * This is the main code that interacts with the server to retrieve new information.  With long polling,
+  * the XHR request is blocked on the server side (and kept open on the client) until there is new data to
+  * send.  It sends the data, closes the connection, and initiates a new connection to wait for data again.
+  * JSONP is used to allow for cross-domain requests.
+  */
+Alerts.prototype.poll = function() { 
+    var _this = this;
+    $.ajax({
+        url: this.url,
+        dataType: 'jsonp',
+        jsonpCallback: 'redalert',
+        success: function(data) {
+            _this.lastSuccess = new Date();
+                        for (var i=0; i < _this.callbacks.length; i++)
+                            _this.callbacks[i](jQuery.parseJSON(data));
+            _this.poll();
+        },
+        error: function(jqXHR, textStatus, errorThrown) {
+            _this.failureCount++;
+            console.log('Error: ' + textStatus + ' ' + errorThrown + ', sleeping...');
+            _this.timeout = setTimeout(function() { _this.poll(); }, _this.sleeptime);
+        }
+    });
+} 
+
+// requestAnimationFrame shim
+// http://www.paulirish.com/2011/requestanimationframe-for-smart-animating/
+var lastTime = 0;
+var vendors = ['webkit', 'moz'];
+for(var x = 0; x < vendors.length && !window.requestAnimationFrame; ++x) {
+    window.requestAnimationFrame = window[vendors[x]+'RequestAnimationFrame'];
+    window.cancelAnimationFrame =
+      window[vendors[x]+'CancelAnimationFrame'] || window[vendors[x]+'CancelRequestAnimationFrame'];
+}
+
+if (!window.requestAnimationFrame)
+    window.requestAnimationFrame = function(callback, element) {
+        var currTime = new Date().getTime();
+        var timeToCall = Math.max(0, 16 - (currTime - lastTime));
+        var id = window.setTimeout(function() { callback(currTime + timeToCall); },
+          timeToCall);
+        lastTime = currTime + timeToCall;
+        return id;
+    };
+
+if (!window.cancelAnimationFrame)
+    window.cancelAnimationFrame = function(id) {
+    clearTimeout(id);
+};
